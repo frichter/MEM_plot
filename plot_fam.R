2cfb3c0bbe069be33c190d985e5bf4e491aba995
frichter <frichter91@gmail.com>
2016-09-16 16:51:13 -0400

    created plots for chr15 region with plot_fam.R

--- plot_fam.R
+++ plot_fam.R
@@ -9,6 +9,7 @@ R
 library(xhmmScripts)
 library(dplyr)
 
+# sshfs richtf01@mothra.hpc.mssm.edu:/hpc/users/richtf01/chdiTrios/Felix/xhmm_test ~/Volumes/xhmm/
 
 plot_prefix = "/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/plot_fam/fam"
 basename = "/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/hg19.all.xhmm"
@@ -18,10 +19,12 @@ JOB_TARGETS_TO_GENES = "/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/hg19.annot
 # load MEM calls
 ################
 
-# ID, CHROM_ME START_ME STOP_ME
+# general file format = ID, CHROM_ME START_ME STOP_ME
+# .chr15_deletions_me.txt
+# .putative_deletion_region_with_me_to_plot.txt
 fam_file_list = list.files("/sc/orga/projects/chdiTrios/Katie/MEM/WES",
-  pattern = "*.putative_deletion_region_with_me_to_plot.txt", full.names = TRUE)
-sample_id_list = lapply(fam_file_list, function(x) gsub("/sc/orga/projects/chdiTrios/Katie/MEM/WES/(.*).putative_deletion_region_with_me_to_plot.txt", "\\1", x))
+  pattern = "*.chr15_deletions_me.txt", full.names = TRUE)
+sample_id_list = lapply(fam_file_list, function(x) gsub("/sc/orga/projects/chdiTrios/Katie/MEM/WES/(.*).chr15_deletions_me.txt", "\\1", x))
 fam_table_list = lapply(fam_file_list, function(x) read.table(x, header = TRUE))
 names(fam_table_list) = sample_id_list
 sample_info_df = fam_table_list %>% bind_rows(.id = "SAMPLE") %>%
@@ -161,31 +164,46 @@ sample_info_df[9:80, ] %>%
 # adjustments for 1p36
 ######################
 
+# 1p36 samples of interest:
 sample_list = c("1-02711", "1-04652", "1-05084", "1-06498", "1-07442")
+
+# chr15 samples of interest: load in first section
+sample_list = sample_info_df %>% filter(avail_in_xhmm) %>% select(SAMPLE) %>% unlist
+names(sample_list) = sample_list
 # sample_re = paste(sample_list, collapse = "|")
 
-sum(TARGETS_CHR_BP1_BP2$bp1 >= 1337395 &
-    TARGETS_CHR_BP1_BP2$bp2 <= 1431087 &
-    TARGETS_CHR_BP1_BP2$chr == "chr1")
+# plotting windows:
+# for 1p36:
+chr_i = "chr1"
+start_i = 1337395
+end_i = 1431087
 
+# chr15 group
+chr_i = "chr15"
+start_i = 22800000 # min(sample_info_df$START) - 1
+end_i = max(sample_info_df$END) + 1
+sum(TARGETS_CHR_BP1_BP2$bp1 >= start_i &
+    TARGETS_CHR_BP1_BP2$bp2 <= end_i &
+    TARGETS_CHR_BP1_BP2$chr == chr_i)
 
-plot_prefix = "/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/plot_fam/1p36/y_upper5/fam"
-PlotFamMEXHMM(sample_list[[1]], "chr1", 1337395, 1431087,
+# plot by FAMILY
+plot_prefix = "/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/chr15/fam_"
+# test on 1 individual
+PlotFamMEXHMM(sample_lo_list_other[[1]], chr_i, start_i, end_i,
   dataList, TARGETS_CHR_BP1_BP2, plot_prefix)
-lapply(sample_list[2:5], function(x)
-  PlotFamMEXHMM(x, "chr1", 1337395, 1431087, dataList, TARGETS_CHR_BP1_BP2, plot_prefix))
+# repeat for all other individuals
+lapply(sample_lo_list_other[[2]], function(x)
+  PlotFamMEXHMM(x, chr_i, start_i, end_i, dataList, TARGETS_CHR_BP1_BP2, plot_prefix))
 
-# plotting all individuals on 1 graph, without parents
-# run PlotFamMEXHMM line-by-line
-
+# plotting all PROBANDS, PARENTS of interest, or both
 PlotSampleList = function(sample_list, CHROM, START, END, dataList,
-                          TARGETS_CHR_BP1_BP2, plot_prefix, basename) {
+                          TARGETS_CHR_BP1_BP2, plot_prefix, basename,
+                          id_index_to_keep) {
     # set desired x axis (ie location on genome)
-    chr = CHROM
-    start_bp = 1337395 #START #- 50000
-    end_bp = 1431087 #END #+ 50000
+    chr = CHROM # chr_i #
+    start_bp = START # start_i # - 50000
+    end_bp = END # end_i # + 50000
     type = "DEL"
-    chr = "chr1"
 
     usePlotName = plot_prefix
 
@@ -195,10 +213,6 @@ PlotSampleList = function(sample_list, CHROM, START, END, dataList,
     dataList[["PCA_NORM_Z_SCORES"]][ dataList[["PCA_NORM_Z_SCORES"]] > ylim_upper_z ] = ylim_upper_z
     # dataList[["PCA_NORM_Z_SCORES"]][ dataList[["PCA_NORM_Z_SCORES"]] < ylim_lower_z ] = ylim_lower_z
 
-    # plot a subset of individuals
-    # probands:
-    # id_index_to_keep = grep("-01$|-02$", row.names(dataList[["PCA_NORM_Z_SCORES"]]), invert = T)
-    id_index_to_keep = grep("-01$|-02$", row.names(dataList[["PCA_NORM_Z_SCORES"]]))
     dataList[["PCA_NORM_Z_SCORES"]] = dataList[["PCA_NORM_Z_SCORES"]][id_index_to_keep, ]
 
     # Obtain target indices: base pair location and position in data of
@@ -248,40 +262,66 @@ PlotSampleList = function(sample_list, CHROM, START, END, dataList,
 }
 
 
+# Choose which individuals to plot (Parents, Probands, or both)
+# only probands:
+id_index_to_keep = grep("-01$|-02$", row.names(dataList[["PCA_NORM_Z_SCORES"]]), invert = T)
+# only parents
+id_index_to_keep = grep("-01$|-02$", row.names(dataList[["PCA_NORM_Z_SCORES"]]))
+# all/both
+id_index_to_keep = grep("", row.names(dataList[["PCA_NORM_Z_SCORES"]]))
 
-plot_prefix = "/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/plot_fam/1p36/proband_only"
-PlotSampleList(sample_list, "chr1", 1337395, 1431087, dataList,
-               TARGETS_CHR_BP1_BP2, plot_prefix, basename)
 
-# identify probands with data in target regions 7-12
+plot_prefix = "/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/chr15/proband_only_zoomed"
+PlotSampleList(sample_list, chr_i, start_i, end_i, dataList,
+               TARGETS_CHR_BP1_BP2, plot_prefix, basename,
+               id_index_to_keep)
+
+
+##########################################################
+# Identify other individuals with deletions in same region
+##########################################################
+
+
+# identify probands with data in specified target regions
+# 1p35: targets 7:12, chr15 region: ???
 # Extract read depth z-scores for these individuals
-sample_df = dataList[["PCA_NORM_Z_SCORES"]][, target_indices[7:12]]
+sample_df = dataList[["PCA_NORM_Z_SCORES"]][, target_indices]
 sample_df = sample_df %>% as.data.frame %>% mutate(Blinded.ID = row.names(sample_df))
 
 # What is the definition of "low"?
-# use highest z-score in MEM probands + 0.5 per exon as cutoff
-sample_list = c("1-02711", "1-04652", "1-05084", "1-06498", "1-07442")
+# Heuristic: use highest z-score in MEM probands + 0.5 per exon as cutoff
+sample_list = c("1-02711", "1-04652", "1-05084", "1-06498", "1-07442") # for 1p36
+sample_list = sample_info_df %>% filter(avail_in_xhmm) %>% select(SAMPLE) %>% unlist # for chr15
+
 blinded_id_interest = sample_df$Blinded.ID %in% sample_list
-perColCutOffs = apply(sample_df[blinded_id_interest, 1:6], 2, max)
+perColCutOffs = apply(sample_df %>% filter(blinded_id_interest) %>% select(-Blinded.ID), 2, max)
 
 # count the number of targets less than the cut-off for probands and parents
-id_index_to_keep = grep("-01$|-02$", row.names(dataList[["PCA_NORM_Z_SCORES"]]))
-cutCount_vec = apply(sample_df[, 1:6], 1, function(pca_values) sum(pca_values <= perColCutOffs + 0.5))
+id_index_to_keep = grep("-01$|-02$", row.names(dataList[["PCA_NORM_Z_SCORES"]]), invert = T)
+cutCount_vec = apply(sample_df %>% select(-Blinded.ID), 1, function(pca_values) sum(pca_values <= perColCutOffs + 0.5))
 sample_df$cutCount_empirical = cutCount_vec
 
-sample_lo_list_proband = sample_df %>%
+sample_lo_list = sample_df %>%
   filter(!grepl("-01$|-02$", Blinded.ID)) %>%
-  filter(cutCount_empirical >= 6 & `chr1:1387418-1387724` < -2) %>%
+  filter(cutCount_empirical >= 60) %>% # for chr15
+  # filter(cutCount_empirical >= 6 & `chr1:1387418-1387724` < -2) %>% # for 1p36
   select(Blinded.ID) %>% unlist
 names(sample_lo_list) = NULL
+sample_lo_list
 # 6: 10, 5: 15, 4: 24
+
+sample_lo_list_other = sample_lo_list[!(sample_lo_list %in% sample_list)]
 
 # sample_lo_list = lapply(sample_lo_list, function(x) as.character(unlist(paste(x, c("-01", "-02"), sep = ""))))
 
-plot_prefix = "/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/plot_fam/1p36/parent_lo"
-PlotSampleList(sample_lo_list, "chr1", 1337395, 1431087, dataList,
+plot_prefix = "/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/chr15/parent_lo"
+PlotSampleList(sample_lo_list, chr_i, start_i, end_i, dataList,
                TARGETS_CHR_BP1_BP2, plot_prefix, basename)
 
+
+###############################
+# plot the "de novos" by family
+###############################
 
 # plot the "de novos" by family
 sample_lo_list_parent = gsub("-0[1-2]$", "", sample_lo_list)
@@ -293,7 +333,7 @@ not_in_parents = lapply(sample_lo_list_proband, function(proband) !(proband %in%
 SAMPLE = sample_lo_list_proband[not_in_parents][[2]]
 sample_list = as.character(unlist(paste(SAMPLE, c("", "-01", "-02"), sep = "")))
 plot_prefix = paste("/hpc/users/richtf01/chdiTrios/Felix/xhmm_test/plot_fam/1p36/fam_", SAMPLE, sep = "")
-PlotSampleList(sample_list, "chr1", 1337395, 1431087, dataList,
+PlotSampleList(sample_list, chr_i, start_i, end_i, dataList,
                TARGETS_CHR_BP1_BP2, plot_prefix, basename)
 
 
